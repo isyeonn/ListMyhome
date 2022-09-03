ceef2a59cdc874d639f036b2e5a7afd5d6647954
isyeon <yean60124@gmail.com>
2022-09-04 00:42:21 +0900

    Spring, Node, Vue 초기셋팅

---
+++ src/main/java/com/isyeon/listmyhome/ListmyhomeApplication.java
@@ -0,0 +1,14 @@
+package com.isyeon.listmyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListmyhomeApplication {
+
+	public static void main(String[] args) {
+		System.out.print("hi, 오랜만~");
+		SpringApplication.run(ListmyhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isyeon/listmyhome/ListmyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isyeon.listmyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListmyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
